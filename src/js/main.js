f44b30efdb43cfe793e1f7035ecd3709157328ba
lod2007 <lod2007@mail.ru>
2019-09-12 20:21:10 +0500

    Работали с Jquery

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,29 @@
-import Counter from './Counter'
-const cnt= new Counter(document.getElementById('counter'))
+ import $ from 'jquery'
+
+$(document).ready(()=>  {
+   $('#title').text('Поменяли текст');
+   $('li.list_item a[data-secret]').on('click',function(e){
+      e.preventDefault();// отменяет обработчик по дефолту
+    console.log($(this).text())
+   })
+
+
+  const square=$('#square');
+  const btn =$('#btn-square');
+
+  btn.on('click',()=>{
+   square.toggleClass('square-soft')
+  })
+
+   
+  square.on('mouseenter',function(){
+      $(this).css({
+        'background-color': 'green',
+         'border-radius': '40px'
+        
+      });
+  })
+
+  })
+
 
